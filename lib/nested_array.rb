28e5614d199ea7866ab71d5025241dafe4f51b19
Matthew Thomas <mzthomas96@gmail.com>
2021-01-03 20:38:52 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -44,7 +44,6 @@ def matrix_update(matrix, row, column, new_value)
   # Update the matrix location at that row and column to have the value of new_value
   # Return the updated matrix
   
-  matrix.delete_at([row][column])
-  matrix.insert([row][column], new_value)
+  matrix[row][column] = [new_value]
   matrix
 end
